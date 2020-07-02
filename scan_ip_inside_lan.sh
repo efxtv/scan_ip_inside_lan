2f78966b0466a02ae66a7b157b80b8b53febd827
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2020-07-02 13:53:35 +0530

    Update scan_ip_inside_lan.sh

--- scan_ip_inside_lan.sh
+++ scan_ip_inside_lan.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ "$1" == "" ]
-then
-echo "You forgot an IP!"
-echo "Syntax: ./ipsweep.sh 192.1.8.0.1"
-
-else
-for ip in `seq 1 254`; do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done
-fi
+clear
+cd cmd
+echo -n "Enter LHOST up to 192.168.0 [HIT ENTER]: "
+read lhosty
+./ip.sh  $lhosty
